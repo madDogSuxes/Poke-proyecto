519f419cce5e9254fff0fff7c0cce909d6e07bfb
madDogSuxes <jesuslorcalopez8@gmail.com>
2023-03-09 11:56:22 +0100

    Añadidos atributos nivel a la clase Pokemon

--- src/pokemone/Pokemon.java
+++ src/pokemone/Pokemon.java
@@ -1,5 +1,15 @@
 package pokemone;
 
 public class Pokemon {
+	private int nivel;
+
+	public int getNivel() {
+		return nivel;
+	}
+
+	public void setNivel(int nivel) {
+		this.nivel = nivel;
+	}
+	
 
 }

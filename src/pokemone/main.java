cece5d0332466e4ffbbc1da3ff7a77f49dc173ee
Miguel Jiménez García <127385416+mmiiki23@users.noreply.github.com>
2023-04-24 14:28:22 +0200

    Añado el JAVAFX
    Madre de dios no he visto cosa más complicada en mi vida.
    P.D. Gracias Luisre

--- src/pokemone/main.java
+++ src/pokemone/main.java
@@ -7,13 +7,13 @@ import java.sql.SQLException;
 public class main {
 	public static void main(String[] args) throws ClassNotFoundException {
 		Connection connection = null;
-		String url = "jdbc:mysql://localhost:3306/Banco ";
+		String url = "jdbc:mysql://localhost:3306/pokemon";
 		String login = "root";
 		String password = "";
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			connection = DriverManager.getConnection(url, login, password);
-			System.out.println("Conexión establecida");
+			System.out.println("Conexion establecida");
 			//Acceso a datos utilizando el objeto de conexión
 		} catch (SQLException sqle) {
 			Throwable e = null;
@@ -21,7 +21,7 @@ public class main {
 		} finally {
 			try {
 				connection.close();
-				System.out.println("Conexión cerrada");
+				System.out.println("Conexion cerrada");
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
